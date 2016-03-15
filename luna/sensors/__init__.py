5aa003bc993e562376161e9f3b920261d1e496d3
Stefano Alberto Russo <stefano.russo@gmail.com>
2016-03-15 17:19:58 +0100

    Finished addding the Sensors. Fixed bug.

--- luna/sensors/__init__.py
+++ luna/sensors/__init__.py
@@ -1,129 +1,94 @@
 from luna.datatypes.composite import DataTimePoint, DataTimeSlot
 from luna.datatypes.composite import PhysicalDataPoint, PhysicalDataSlot, DimensionalDataTimePoint, PhysicalData, PhysicalDataTimeSlot
 from luna.datatypes.composite import PhysicalDataTimePoint
-from luna.datatypes.dimensional import TimeSlot
-from luna.spacetime.time import TimeInterval
+from luna.datatypes.dimensional import TimeSlot, TimePoint
+from luna.spacetime.time import TimeSlotSpan
 from luna.aggregators import operations
 
 
 class Sensor(object):
-    '''This is a  base class that define a sensor. In Luna, a Sensor is a thing that produce some data, and ca represent
-    whatever you feel like - a Sensor, a log collector, a webcam.. 
+    '''This is a  base class that define a sensor. In Luna, a Sensor is
+    just a thing that produce some data.
     '''
     
-    # An instantiated sensor will have a (unique) id
+    # An instantiated sensor will have a (unique) id. This is also
+    # necessary for storing data (it is used as a primary key)
     def __init__(self, id):
         self.id = id
         
     def __repr__(self):
         return '{} - {}'.format(self.__class__.__name__, self.id)
     
-    # A sensor has its own type_ID and prodeuce some data:    
+    # A sensor has its own type_ID and produce some data:    
         
     @property
-    def type_ID(self): #TODO: Sure!?
+    def type_ID(self):
         raise NotImplementedError
 
     @property
-    def data_type(self):
+    def Points_type(self):
         raise NotImplementedError
 
-    # .. and for now they are not implemented and that's it.
+    @property
+    def Slots_type(self):
+        raise NotImplementedError    
     
 
-class TimeBasedSensor(Sensor):
-    ''' A time-based sensor is a sensor thta produces time-based data.'''
+class TimeSensor(Sensor):
+    ''' A time-based sensor is a sensor that produces only time events. A plausible example
+    could be a person counter at a shop entrance, and  and it would be typically bound with 
+    a TimeSeries (note: not a DataTimeSeries). It is anyway not a real-world example, as some data
+    would be always be attached to an event at the end (a '1' in the above example, for instance)'''
+
+    Points_type = TimePoint
+    Slots_type  = TimeSlot
+
     
-    data_type = DataTimePoint # Could alse be DataTimeSlot
-    
-    
-class TimeBasedDimensionalDataSensor(TimeBasedSensor):
-    '''This Sensor is menat to produce dimensional data. DimensionalData is menat to be int or float data, and the most common
-    example is latituted and longitude'''
-    
-    data_type = DimensionalDataTimePoint
-    
+class DataTimeSensor(TimeSensor):
+    '''A time-based data sensor is a sensor that produces time-based data.'''
+
+    # Here we set that Points and slots must be carry data, by setting the type...
+    Points_type = DataTimePoint
+    Slots_type  = DataTimeSlot
+
+    # ...but we do not say anything about the labels that will be used to actually label the data.
+
     @property
     def Points_data_labels(self):
-        raise NotImplementedError        
+        raise NotImplementedError
 
-    # We can define common operations on Dimensional data, as MIN, MAX, AVG when we aggregate Points in Slots:
-    Points_data_to_Slots_data_ops = ['MIN', 'MAX', 'AVG']
-
-    # We can let extension of this class to have extra (and user-defined) ops:
-    Points_data_to_Slots_data_extra_ops =[]
-
-    # We also need a mapping telling which operations have to be applied on which label, and which label generates.
-    # By defualt, whe apply the ops to each label
-
-    # Note: Once a slot is created, the operations are not modifiable wnymore whan aggregating from Slots to a bigger Slot.
-    # i.e. if you create MIN MAX AVG when aggregatting from Points to Slots, you will always carry them in the aggregations
-
-    # By default, we apply the Points_to_Slots_ops to every label
-    
-    # TODO: this mapping has to be a MATRIX!!!
-   
-    Points_to_Slots_ops_mapping = {}
-    '''
-    power_W -> power_W_MIN, power_W_MAX, power_W_AVG
-    power_W_peaks
-    '''
-    #Points_to_Slots_ops_mapping = {label:Points_to_Slots_ops_mapping for label in Points_data_labels}
-
-
-    # A DimensionalDataSensor one aggregated in a Slot has some Slot dat labels once aggregated:
     @property
     def Slots_data_labels(self):
-        Slots_data_labels = ['....compute them....']
-        return Slots_data_labels
-        
+        raise NotImplementedError
 
-
-
-#------------------------------------
-# We restart from here
-#------------------------------------
-
-
-   
-class TimeBasedPhysicalDataSensor(TimeBasedSensor):
-    '''This Sensor is menat to produce physical data. PhysicalData is a particular type of Dimensional Data where each dimensiona
-    has to be a valid PhysicalQuantity. A common example is a Sensor which monitor water flow and temperature: The PhysicalData would be
-    bi-dimensional (one dimension is the flow, the second dimesnion is the temperature). The TimeBasedPhysicalDataSensor knows how to aggrgeate
-    intensive and extensive metrics.'''
+    # The validity interval value has also to be set. It is per-sensor,
+    # as it is assumed that every physical sensor behaves in the same way.
+    # You can also set it to None and use a dynamic (per Point) validty
+    # interval according to the storage, but this feature is still in alpha phase.
+    # The validity interval is a very delicate concept to be crefully evaluated
+    # according to the discretization of the continuum space, please refer
+    # to the documentation for more informations
     
-    # TODO: handle intensive/extensive here?
+    @property
+    def Points_validity_interval(self):
+        raise NotImplementedError  
+    
+class PhysicalDataTimeSensor(DataTimeSensor):
+    '''A time-based data sensor is a sensor that produces time-based physical data.'''
+
+    # Here we set that Points and slots must be carry physical data
     Points_type = PhysicalDataTimePoint
     Slots_type  = PhysicalDataTimeSlot
-
-class VolumetricSensorV1(TimeBasedPhysicalDataSensor):
-
-    # Assign unique type_ID to thing type
-    type_ID = 5
-
-    # Set (override) data_content_Points_labels
-    Points_data_labels = ['flowrate_m3s'] #Mandatory
-    Slots_data_labels  = ['flowrate_m3s_i_AVG', 'flowrate_m3s_i_MIN', 'flowrate_m3s_i_MAX', 'volume_m3_e_TOT'] # TODO: compute them automatically according to operations
     
-    # Validty interval
-    Points_validity_interval =  TimeInterval('60s') # Validty Slot type, or don't set it or set it to None if you implement it in the storage to have variable validity
+    # ..the lables has still to be set.
 
+    # Plus, we have a call to understand the minimal set of PhysicalQuantities required
+    # So the ones from which everything else is computed (Sure?).. at least for the slot..
     
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# For and example usage, see the unit test of this package,
+# where you will find a VolumetricSensorV1 sensor implemented.
 
 
 
---
+++ luna/sensors/tests/test_sensors.py
@@ -0,0 +1,111 @@
+import unittest
+from luna.datatypes.composite import DataTimeSeries, DataTimePoint, PhysicalDataTimePoint, PhysicalDataTimeSlot, StreamingDataTimeSeries
+from luna.datatypes.dimensional import *
+from luna.common.exceptions import InputException, StorageException
+from luna.spacetime.time import dt, TimeSlotSpan
+from luna.datatypes.dimensional import TimePoint, Point, PhysicalData
+from luna.sensors import PhysicalDataTimeSensor
+from luna.storage.sqlite import sqlite
+
+
+# TODO: this should be a generic storgae test, which is applied to the various modules (whcih can be write or read only)
+
+
+#------------------------------------
+# Logging
+#------------------------------------
+
+# Enable only critical logging in unit tests by default
+logging.basicConfig(level=logging.CRITICAL) 
+logger = logging.getLogger("luna")
+
+
+#------------------------------------
+# Tests
+#------------------------------------
+
+class test_sensors(unittest.TestCase):
+
+    def setUp(self):       
+        pass
+
+
+    def test_define_new_Sensor_Volumetric(self):
+        
+        class VolumetricSensorV1(PhysicalDataTimeSensor):
+
+            # Assign unique type_ID to this sensor type
+            type_ID = 5
+        
+            # Set Points expected lables
+            Points_data_labels = ['flowrate_m3s']
+            
+            # Set Slots operators
+            Slots_data_labels  = ['flowrate_m3s_AVG', 'flowrate_m3s_MIN', 'flowrate_m3s_MAX', 'volume_m3_TOT'] 
+            
+            # Set Validty interval
+            Points_validity_interval =  TimeSlotSpan('60s')
+
+        # Test volumetric sensor
+        volumetricSensorV1_1 = VolumetricSensorV1('lu65na')
+        volumetricSensorV1_2 = VolumetricSensorV1('lu34na')
+        
+        self.assertEqual(volumetricSensorV1_1.id, 'lu65na')
+        self.assertEqual(volumetricSensorV1_2.id, 'lu34na')
+        
+        self.assertEqual(volumetricSensorV1_1.Points_data_labels, ['flowrate_m3s'])
+        self.assertEqual(volumetricSensorV1_2.Slots_data_labels, ['flowrate_m3s_AVG', 'flowrate_m3s_MIN', 'flowrate_m3s_MAX', 'volume_m3_TOT'])
+
+
+    def test_define_new_Sensor_Electric(self):
+        
+        class ElectricEnergySensorV1(PhysicalDataTimeSensor):
+
+            # Assign unique type_ID to this sensor type
+            type_ID = 6
+        
+            # Set Points expected lables
+            Points_data_labels = ['power_W']
+            
+            # Set Slots operators
+            Slots_data_labels  = ['power_W_AVG', 'power_W_MIN', 'power_W_MAN', 'energy_kWh_TOT'] 
+            
+            # Set Validity interval
+            Points_validity_interval =  TimeSlotSpan('60s')
+
+            # Define custom operation required for energy_kWh_TOT:
+            def energy_kWh(self, points_dataTimeSeries, slot_dataTimeSerie):
+                return slot_dataTimeSerie
+        
+        
+        electricEnergySensorV1 = ElectricEnergySensorV1('0112233445566/1-MB-3')
+        
+        # Check ID
+        self.assertEqual(electricEnergySensorV1.id, '0112233445566/1-MB-3') 
+        
+        
+    def tearDown(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
